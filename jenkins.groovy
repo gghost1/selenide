71f9539989a2ed65e845146c33ea0df8caea2ab8
GG_Host <gd.host@yandex.ru>
2025-03-24 01:48:57 +0300

    fix

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,18 +1,19 @@
-task_branch = "${TEST_BRANCH_NAME}"
-def brunch_cutted = task_branch.contains("origin/") ? task_branch.split("origin/")[1] : task_branch.trim()
-currentBuild.displayName = "$brunch_cutted"
+def task_branch = "${TEST_BRANCH_NAME}"
+def branch_cutted = task_branch.contains("origin/") ? task_branch.split("origin/")[1] : task_branch.trim()
+currentBuild.displayName = "${branch_cutted}"
 
-base_git_url = "https://github.com/gghost1/selenide.git"
+def base_git_url = "https://github.com/gghost1/selenide.git"
 
 node {
-    withEnv(["branch=${brunch_cutted}", "base_url=${base_git_url}"]) {
+    withEnv(["branch=${branch_cutted}", "base_url=${base_git_url}"]) {
         stage('Checkout Branch') {
-            if (!"$branch_cutted".contains("master")) {
+            if (!env.branch.contains("master")) {
                 try {
-                    getProject("$base_url", "$branch_cutted")
+                    getProject("${base_git_url}", "${env.branch}")
                 } catch (Exception e) {
-                    echo "Branch $branch_cutted not found"
-                    throw ("$e")
+                    echo "Branch ${env.branch} not found"
+                    currentBuild.result = 'FAILURE'
+                    error(e.toString())
                 }
             } else {
                 echo "Checkout master branch"
@@ -24,14 +25,13 @@ node {
         } finally {
             generateAllure()
         }
-
     }
 }
 
 def getTestStages(testTags) {
     def stages = [:]
-    testStages.each {
-        tag -> stages["${tag}"] = {
+    testTags.each { tag ->
+        stages["${tag}"] = {
             returnTestWithTag(tag)
         }
     }
@@ -39,20 +39,19 @@ def getTestStages(testTags) {
 }
 
 def returnTestWithTag(String tag) {
-    try {
-        labelledShell(label: "Run $tag", script: "mvn clean test -Dtest=$tag")
-    } catch (Exception e) {
-        echo "Test $tag failed"
+    catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
+        labelledShell(label: "Run ${tag}", script: "mvn clean test -Dtest=${tag}")
     }
 }
 
 def getProject(String repo, String branch) {
     cleanWs()
-    checkout scm: [
+    checkout([
             $class: 'GitSCM',
             branches: [[name: branch]],
+            extensions: [],
             userRemoteConfigs: [[url: repo]]
-    ]
+    ])
 }
 
 def generateAllure() {
