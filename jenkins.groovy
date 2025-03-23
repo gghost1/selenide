bb355af0a38e558a7713f3b72abf402818041b55
GG_Host <gd.host@yandex.ru>
2025-03-24 02:12:36 +0300

    fix

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,30 +1,28 @@
-def task_branch = "${TEST_BRANCH_NAME}"
-def branch_cutted = task_branch.contains("origin/") ? task_branch.split("origin/")[1] : task_branch.trim()
-currentBuild.displayName = "${branch_cutted}"
-
-def base_git_url = "https://github.com/gghost1/selenide.git"
+def mvnHome = tool name: 'Maven', type: 'maven'
 
 node {
     withEnv(["branch=${branch_cutted}", "base_url=${base_git_url}"]) {
         stage('Checkout Branch') {
-            if (!env.branch.contains("master")) {
-                try {
-                    getProject("${base_git_url}", "${env.branch}")
-                } catch (Exception e) {
-                    echo "Branch ${env.branch} not found"
-                    currentBuild.result = 'FAILURE'
-                    error(e.toString())
+            script {
+                if (!env.branch.contains("master")) {
+                    checkout([
+                            $class: 'GitSCM',
+                            branches: [[name: env.branch]],
+                            extensions: [],
+                            userRemoteConfigs: [[url: env.base_url]]
+                    ])
+                } else {
+                    checkout scm
                 }
-            } else {
-                getProject("${base_git_url}", "master")
-                echo "Checkout master branch"
             }
         }
 
-        try {
-            parallel getTestStages(["ImageTest"])
-        } finally {
-            generateAllure()
+        stage('Run Tests') {
+            try {
+                parallel getTestStages(["ImageTest"])
+            } finally {
+                generateAllure()
+            }
         }
     }
 }
@@ -41,25 +39,6 @@ def getTestStages(testTags) {
 
 def returnTestWithTag(String tag) {
     catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-        labelledShell(label: "Run ${tag}", script: "mvn clean test -Dtest=${tag}")
+        sh "${mvnHome}/bin/mvn clean test -Dtest=${tag}"
     }
-}
-
-def getProject(String repo, String branch) {
-    checkout([
-            $class: 'GitSCM',
-            branches: [[name: branch]],
-            extensions: [],
-            userRemoteConfigs: [[url: repo]]
-    ])
-}
-
-def generateAllure() {
-    allure([
-            includeProperties: true,
-            jdk: '',
-            properties: [],
-            reportBuildPolicy: 'ALWAYS',
-            results: [[path: 'target/allure-results']]
-    ])
 }
